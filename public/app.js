a678fde3b2da37040729fb2bdd20c49fe6837838
Vipurthanan <lessandsass@gmail.com>
2025-01-13 19:33:43 +0530

    - Bug fixed
    - Bug fix the public\app.css file was moved into public\css\ folder
    - Some markup was added in the posts.blade.php file
    - some styles was added in the app.css file

--- public/app.js
+++
@@ -1,3 +0,0 @@
-alert('I am here');
-
-
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+    <article>
+        <h1>
+            My first Post
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. In incidunt velit, earum dolorem provident culpa praesentium, suscipit deserunt iste vel ducimus harum cupiditate mollitia accusantium! Rem dicta veniam voluptatibus in?
+        </p>
+    </article>
+
+    <article>
+        <h1>
+            My second Post
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. In incidunt velit, earum dolorem provident culpa praesentium, suscipit deserunt iste vel ducimus harum cupiditate mollitia accusantium! Rem dicta veniam voluptatibus in?
+        </p>
+    </article>
+
+    <article>
+        <h1>
+            My Third Post
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. In incidunt velit, earum dolorem provident culpa praesentium, suscipit deserunt iste vel ducimus harum cupiditate mollitia accusantium! Rem dicta veniam voluptatibus in?
+        </p>
+    </article>
+
+</body>
+</html>
+
+
+
