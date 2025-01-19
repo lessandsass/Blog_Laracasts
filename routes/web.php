9847dc2f0778dc665650140e5995e2056b012d2f
Vipu <lessandsass@gmail.com>
2025-01-19 19:22:59 +0530

    Caching
    - Every time accessing the file system is an expensive operation.
    - Unique key was passed inside the remember method.
    - Then the amount of time to hold the caching was passed just after the unique key.
    - We can’t use the $path variable inside the closure.
    - There are two way to fix that.
    - use key word and arrow function.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ Route::get('posts/{post}', function ($slug) {
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(5), function () use ($path) {
+        var_dump('file_get_contents');
+        return file_get_contents($path);
+    });
+
     return view('post', [
         'post' => $post
     ]);
