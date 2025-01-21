5e37b13dc2824b73eee183dcdd1f832c58b10076
Vipu <lessandsass@gmail.com>
2025-01-21 18:01:18 +0530

    Refactor
    - The logic in the route file was pasted over in the Post.php model refactor the code.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 class Post
 {
-    public static function find()
+    public static function find($slug)
     {
+        $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
+        if (!file_exists($path)) {
+            return redirect('/');
+        }
+
+        return cache()->remember("posts.{$slug}", 3600, function () use ($path) {
+            return file_get_contents($path);
+        });
     }
 }
 
