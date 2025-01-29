b2148961d076322722cfb24befb232d9daffbba7
Vipu <lessandsass@gmail.com>
2025-01-29 19:50:21 +0530

    Iterate the files in the resource path
    - we can’t pass the document objects to the views, instead of that we can pass the $post object
    - It will throw an error, to fix that we can make a constructor in the Post class above the all function
    - Now we can call the attribute of the document object.
    - we can get the first post of an array
    - we can extract the attributes
    - we can pass that through the view

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,30 @@
 <?php
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
 
-    $document = YamlFrontMatter::parseFile(
-        resource_path('posts/my-first-post.html'),
-    );
+    $files = File::files(resource_path("posts/"));
 
-    dd($document->title);
+    $posts = [];
 
-    // return view('posts', [
-    //     'posts' => Post::all()
-    // ]);
+    foreach ($files as $file) {
+        $document = YamlFrontMatter::parseFile($file);
+
+        $posts[] = new Post (
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+        );
+    }
+
+    return view('posts', [
+        'posts' => $posts
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
