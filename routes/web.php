dada371afbfb2ee38f9d1128e088d194ffeb2622
Vipu <lessandsass@gmail.com>
2025-01-29 18:36:20 +0530

    extracting values using Yaml front matter
    - The contents of the file was pulled in using the YamlFrontMatter class in the route file.
    - You can dd the document object
    - We can extract the body object.
    - We can extract the matter attributes
    - we also has the magic property access

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,19 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::all()
-    ]);
+
+    $document = YamlFrontMatter::parseFile(
+        resource_path('posts/my-first-post.html'),
+    );
+
+    dd($document->title);
+
+    // return view('posts', [
+    //     'posts' => Post::all()
+    // ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
