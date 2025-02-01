0b9bf9de60ece82974d2afbbc5eb9cb32188439f
Vipu <lessandsass@gmail.com>
2025-02-01 12:53:26 +0530

    Arrow functions
    - Inline the mapping using Arrow functions

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
-
     $posts = collect(File::files(resource_path("posts/")))
-        ->map(function ($file) {
-            return YamlFrontMatter::parseFile($file);
-        })
-        ->map(function ($document) {
-            return new Post (
+        ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn ($document) =>
+            new Post (
                 $document->title,
                 $document->excerpt,
                 $document->date,
                 $document->body(),
                 $document->slug
-            );
-        });
+            )
+        );
 
     return view('posts', [
         'posts' => $posts
