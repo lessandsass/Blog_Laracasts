0b9df157ec3f2906e61449a1c78aa148767dbc46
Vipu <lessandsass@gmail.com>
2025-02-01 16:10:45 +0530

    Alter the find method
    - use the all( ) static method to grab the post which matches the slug
    - The post view file was changed as to grab the post object attributes.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -41,15 +41,7 @@ class Post
 
     public static function find($slug)
     {
-        $path = resource_path("posts/{$slug}.html");
-
-        if (!file_exists($path)) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 3600, function () use ($path) {
-            return file_get_contents($path);
-        });
+        return static::all()->firstWhere('slug', $slug);
     }
 }
 
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,12 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog Laracasts</title>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="../css/app.css">
 </head>
 <body>
 
     <article>
-        <?= $post;  ?>
+        <h1>
+            <?= $post->title; ?>
+        </h1>
+
+        <div>
+            <?= $post->body; ?>
+        </div>
     </article>
 
     <a href="/">Go Back</a>
