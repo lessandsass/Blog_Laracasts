07900fb1fd38127b6464c20ba852493be63a86bb
Vipu <lessandsass@gmail.com>
2025-02-01 20:55:27 +0530

    15. Layout-Files-Two-Ways
    - The layout file was created inside the views directory
    - The banner section was yielded in the layout file

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog Laracasts</title>
-    <link rel="stylesheet" href="../css/app.css">
-</head>
-<body>
+@extends('layout')
+
+@section('content')
 
     <article>
         <h1>
@@ -21,8 +14,4 @@
 
     <a href="/">Go Back</a>
 
-</body>
-</html>
-
-
-
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog Laracasts</title>
-    <link rel="stylesheet" href="css/app.css">
-</head>
-<body>
+@extends('layout')
 
+@section('banner')
+    <h1>
+        Blog 
+    </h1>
+@endsection
+
+@section('content')
     @foreach ($posts as $post)
     <article>
         <h1>
@@ -22,9 +20,6 @@
         </div>
     </article>
     @endforeach
-
-</body>
-</html>
-
+@endsection
 
 
