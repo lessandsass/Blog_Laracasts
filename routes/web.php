8f2720c2de8d4aa293210cbcdbc08f2be3b31226
Vipu <lessandsass@gmail.com>
2025-02-01 12:40:29 +0530

    Laravel collection syntax
    - The collection syntax was added
    - $files variable was made as inline

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,18 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
 
-    $files = File::files(resource_path("posts/"));
+    $posts = collect(File::files(resource_path("posts/")))
+        ->map(function ($file) {
+            $document = YamlFrontMatter::parseFile($file);
 
-    $posts = array_map(function ($file) {
-        $document = YamlFrontMatter::parseFile($file);
-
-        return new Post (
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        );
-    }, $files);
+            return new Post (
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            );
+        });
 
     return view('posts', [
         'posts' => $posts
