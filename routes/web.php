2bcee835e0ba9f0dcb83e7f44297a6202cc11486
Vipu <lessandsass@gmail.com>
2025-03-12 17:22:37 +0530

    Route Model Binding
    - We are binding a route key underlying eloquent model.
    - The wild card name have to be similar to the variable name.
    - Then Laravel find the post with given id

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function ($id) {
+Route::get('posts/{post}', function (Post $post) {
     return view('post', [
-        'post' => Post::findOrFail($id)
+        'post' => $post
     ]);
 });
 
